9798ab497a1e8a111f4a1e3098c0955ab3b4f0fd
Bolza <stefano.sergio@gmail.com>
2017-01-22 19:18:05 +0000

    starting from the base of the jetroid game

---
+++ Assets/Jetroid/Scripts/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+	public float speed = 10f;
+	private Rigidbody2D body;
+	private bool standing = false;
+
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		body.velocity = new Vector2 (transform.localScale.x * speed, body.velocity.y);
+	}
+}
---
+++ Assets/Jetroid/Scripts/SpawnPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+	public GameObject creature;
+	private Transform tr;
+	// Use this for initialization
+	void Start () {
+		tr = this.transform;
+		StartCoroutine(InvokeMethod(create, 1f, 5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void create()
+	{
+		Debug.logger.Log (tr.position);
+		Instantiate (creature, tr.position, Quaternion.identity);
+	}
+
+	public IEnumerator InvokeMethod(Action method, float interval, int invokeCount)
+	{
+		for (int i = 0; i < invokeCount; i++)
+		{
+			method();
+
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
